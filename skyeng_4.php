e4afdf5a319511ff6f77f1056a5d0a158116fdfd
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-09-10 21:10:46 +0000

    123

--- skyeng_4.php
+++ skyeng_4.php
@@ -10,13 +10,29 @@ function bigSum(string $firstComponent, string $secondComponent): string
         $secondComponent = $tmpFirstComponent;
     }
     while (strlen($firstComponent) > 0) {
-        $firstComponentPart = (int) substr($firstComponent, $position, 9);
+        $firstComponentStr = substr($firstComponent, $position, 9);
+        $firstComponentPart = (int) $firstComponentStr;
         $secondComponentPart = (int) substr($secondComponent, $position, 9);
         $sum = $firstComponentPart + $secondComponentPart + $sumOverflow;
+        $firstComponentStrLength = strlen($firstComponentStr);
+        $sumStrLength = strlen($sum);
+        for ($i = 0; $i < $firstComponentStrLength - $sumStrLength; ++$i) {
+            $sum = '0' . $sum;
+        }
         $sumOverflow = (int) substr($sum, 0, -9);
         $sum = substr($sum, -9, 9);
-        $firstComponent = substr($firstComponent, 0, strlen($firstComponent) - 9);
-        $secondComponent = substr($secondComponent, 0, strlen($secondComponent) - 9);
+        $firstComponentStrlen = strlen($firstComponent) - 9;
+        $secondComponentStrlen = strlen($secondComponent) - 9;
+        if ($firstComponentStrlen < 1) {
+            $firstComponent = '';
+        } else {
+            $firstComponent = substr($firstComponent, 0, $firstComponentStrlen);
+        }
+        if ($secondComponentStrlen < 1) {
+            $secondComponent = '';
+        } else {
+            $secondComponent = substr($secondComponent, 0, $secondComponentStrlen);
+        }
         $result = $sum . $result;
     }
     return $result;
